424cb23e0801d47cb1a50ffb602df8028895115d
MrPotatoJr <90863275+MrPotatoJr@users.noreply.github.com>
2025-01-13 03:17:40 +0000

    Primer Commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,45 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+    const [createTask, setTask] = useState('');
+    const [listTask, setPendingTask] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    return (
+        <div className="text-center my-3">
+            <h1>To do list</h1>
+            {setTask}
+            <div className="d-flex row">
+                <input
+                    className="form-control m-5"
+                    type="text"
+                    value={createTask}
+                    onChange={(event) => {
+                        setTask(event.target.value);
+                    }}
+                    onKeyDown={(event) => {
+                        if (event.key === "Enter") {
+                            setPendingTask([...listTask, createTask]);
+                            setTask("");
+                        }
+                    }}
+                />
+                {listTask.map(task => (
+                    
+					<div><p
+                        style={{ border: '1px solid black' }}
+                        key={task}
+                    >
+                        {task}
+                    </p>
+					
+					</div>
+					
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Home;
